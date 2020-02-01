dd7f8198197afc36fa267c208b1f7afa0f9ddaff
Eric Cornelissen <ericornelissen@gmail.com>
2020-02-01 13:21:22 +0200

    test: create simple tests for getPrNumber

--- test/main.test.ts
+++ test/main.test.ts
@@ -4,7 +4,7 @@ import * as github from "@actions/github";
 jest.mock('@actions/core', () => require('./mocks/@actions/core'));
 jest.mock('@actions/github', () => require('./mocks/@actions/github'));
 
-import { getChangedFiles } from "../src/github-api";
+import { getChangedFiles, getPrNumber, PR_NOT_FOUND } from "../src/github-api";
 
 
 const PR_WITH_NO_CHANGES: number = 1;
@@ -24,6 +24,20 @@ describe("GitHub API::getChangedFiles", () => {
   it("returns correctly for a Pull Request with no changes", async () => {
     const changedFiles = await getChangedFiles(client, PR_WITH_NO_CHANGES);
     expect(changedFiles).toBeDefined();
+  });
+
+  it.each([1, 2, 5, 46])("returns the correct number for Pull Request #%i", (a: number) => {
+    github.context.payload.pull_request = { number: a };
+
+    const actual: number = getPrNumber();
+    expect(actual).toBe(a);
+  });
+
+  it("returns PR_NOT_FOUND when there was no Pull Request in the context", () => {
+    github.context.payload.pull_request = undefined;
+
+    const actual: number = getPrNumber();
+    expect(actual).toBe(PR_NOT_FOUND);
   });
 
 });
