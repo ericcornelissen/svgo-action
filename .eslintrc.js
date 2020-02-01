4ca99e6b50b6cd5f0ec771955705edcfdd73add8
Eric Cornelissen <ericornelissen@gmail.com>
2020-02-02 00:13:49 +0200

    chore: add ESLint for TypeScript and JavaScript

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+
+  ignorePatterns: [
+    "node_modules/", // dependencies directory
+    "lib/", // TypeScript generated files
+  ],
+
+  globals: {
+    "jest": "readonly",
+    "module": "readonly",
+  },
+  rules: {
+    "comma-dangle": ["error", "always"],
+  },
+};
