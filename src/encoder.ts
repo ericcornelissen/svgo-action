a42223627a430b368d37af61df7500099e6ae894
Eric Cornelissen <ericornelissen@gmail.com>
2020-02-08 19:21:31 +0200

    feat: encode strings to Base64

--- src/encoder.ts
+++ src/encoder.ts
@@ -1,10 +1,16 @@
+const UNKNOWN_ENCODING_MSG = "Unknown file encoding";
+
 const BASE64 = "base64";
 const UTF8 = "utf-8";
-
 
 function decodeBase64(data: string): string {
   const dataBuffer = Buffer.from(data, BASE64);
   return dataBuffer.toString(UTF8);
+}
+
+function encodeBase64(data: string): string {
+  const dataBuffer = Buffer.from(data, UTF8);
+  return dataBuffer.toString(BASE64);
 }
 
 
@@ -12,6 +18,14 @@ export function decode(data: string, fileEncoding: string): string {
   if (fileEncoding === BASE64) {
     return decodeBase64(data);
   } else {
-    throw Error("Unknown file encoding");
+    throw Error(UNKNOWN_ENCODING_MSG);
+  }
+}
+
+export function encode(data: string, fileEncoding: string): string {
+  if (fileEncoding === BASE64) {
+    return encodeBase64(data);
+  } else {
+    throw Error(UNKNOWN_ENCODING_MSG);
   }
 }
--- test/encoder.test.ts
+++ test/encoder.test.ts
@@ -1,4 +1,4 @@
-import { decode } from "../src/encoder";
+import { encode, decode } from "../src/encoder";
 
 
 describe("::decode", () => {
@@ -13,6 +13,24 @@ describe("::decode", () => {
     ])("Decode a base64 string (%s)", (base64String: string, utf8String: string) => {
       const result: string = decode(base64String, "base64");
       expect(result).toBe(utf8String);
+    });
+
+  });
+
+});
+
+describe("::encode", () => {
+
+  describe("Base64", () => {
+
+    test.each([
+      ["Hello world!", "SGVsbG8gd29ybGQh"],
+      ["banana for scale", "YmFuYW5hIGZvciBzY2FsZQ=="],
+      ["Another one bites de_dust", "QW5vdGhlciBvbmUgYml0ZXMgZGVfZHVzdA=="],
+      ["The Spanish Inquisition", "VGhlIFNwYW5pc2ggSW5xdWlzaXRpb24="],
+    ])("Encode a base64 string (%s)", (utf8String: string, base64String: string) => {
+      const result: string = encode(utf8String, "base64");
+      expect(result).toBe(base64String);
     });
 
   });
