230a4042a934ba04da058d722c462a13e2341d2b
Eric Cornelissen <ericornelissen@gmail.com>
2020-02-08 19:39:18 +0200

    test: bad weather tests for encoding/decoding

--- test/encoder.test.ts
+++ test/encoder.test.ts
@@ -5,16 +5,29 @@ describe("::decode", () => {
 
   describe("Base64", () => {
 
+    const BASE64 = "base64";
+
     test.each([
       ["SGVsbG8gd29ybGQh", "Hello world!"],
       ["R3VwcHk=", "Guppy"],
       ["V2h5IG5vdCBab2lkYmVyZw==", "Why not Zoidberg"],
       ["Tm8gdGhpcyBpcyBQYXRyaWNr", "No this is Patrick"],
     ])("Decode a base64 string (%s)", (base64String: string, utf8String: string) => {
-      const result: string = decode(base64String, "base64");
+      const result: string = decode(base64String, BASE64);
       expect(result).toBe(utf8String);
     });
 
+    test("Decoding an empty base64 string", () => {
+      const emptyString = "";
+
+      const result: string = decode(emptyString, BASE64);
+      expect(result).toBe(emptyString);
+    });
+
+  });
+
+  test("throw for unknown encoding", () => {
+    expect(() => decode("SGVsbG8gd29ybGQh", "foobar")).toThrow();
   });
 
 });
@@ -23,16 +36,29 @@ describe("::encode", () => {
 
   describe("Base64", () => {
 
+    const BASE64 = "base64";
+
     test.each([
       ["Hello world!", "SGVsbG8gd29ybGQh"],
       ["banana for scale", "YmFuYW5hIGZvciBzY2FsZQ=="],
       ["Another one bites de_dust", "QW5vdGhlciBvbmUgYml0ZXMgZGVfZHVzdA=="],
       ["The Spanish Inquisition", "VGhlIFNwYW5pc2ggSW5xdWlzaXRpb24="],
     ])("Encode a base64 string (%s)", (utf8String: string, base64String: string) => {
-      const result: string = encode(utf8String, "base64");
+      const result: string = encode(utf8String, BASE64);
       expect(result).toBe(base64String);
     });
 
+    test("Encoding an empty string to base64", () => {
+      const emptyString = "";
+
+      const result: string = encode(emptyString, BASE64);
+      expect(result).toBe(emptyString);
+    });
+
+  });
+
+  test("throw for unknown encoding", () => {
+    expect(() => encode("Hello world!", "foobar")).toThrow();
   });
 
 });
