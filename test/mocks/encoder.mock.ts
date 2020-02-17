f24b7afba35855e4f92630d8139e29f27e2f2140
Eric Cornelisesn <ericornelissen@gmail.com>
2020-02-17 10:47:27 +0200

    refactor: fix indentation in encoder.mock.ts & svgo.mock.ts

--- test/mocks/encoder.mock.ts
+++ test/mocks/encoder.mock.ts
@@ -1,10 +1,12 @@
 import contentPayloads from "../fixtures/contents-payloads.json";
 import svgs from "../fixtures/svgs.json";
 
-export const decode = jest.fn((data, encoding) => {
+export const decode = jest.fn()
+  .mockImplementation((data, encoding) => {
     for (const [filename, payload] of Object.entries(contentPayloads)) {
-        if (payload.content === data && payload.encoding === encoding) {
-            return svgs[filename];
-        }
+      if (payload.content === data && payload.encoding === encoding) {
+        return svgs[filename];
+      }
     }
-}).mockName("decode");
+  })
+  .mockName("decode");
--- test/mocks/svgo.mock.ts
+++ test/mocks/svgo.mock.ts
@@ -2,5 +2,8 @@ export const optimizerInstance = {
   optimize: jest.fn().mockName("optimize"),
 };
 
-const SVGOptimizer = jest.fn().mockReturnValue(optimizerInstance).mockName("SVGOptimizer");
+const SVGOptimizer = jest.fn()
+  .mockReturnValue(optimizerInstance)
+  .mockName("SVGOptimizer");
+
 export default SVGOptimizer;
