95eec798e3833824ae22ed1d5b62013a8b4e22e2
Eric Cornelisesn <ericornelissen@gmail.com>
2020-02-17 16:17:22 +0200

    refactor: template literals for encoding name in encoder test suite

--- test/encoder.test.ts
+++ test/encoder.test.ts
@@ -7,19 +7,19 @@ const EMPTY_STRING = "";
 
 describe("::decode", () => {
 
-  describe("Base64", () => {
+  describe(`Decoding from ${BASE64}`, () => {
 
     test.each([
       ["SGVsbG8gd29ybGQh", "Hello world!"],
       ["R3VwcHk=", "Guppy"],
       ["V2h5IG5vdCBab2lkYmVyZw==", "Why not Zoidberg"],
       ["Tm8gdGhpcyBpcyBQYXRyaWNr", "No this is Patrick"],
-    ])("Decode a base64 string (%s)", (base64String: string, utf8String: string) => {
+    ])(`Decode a ${BASE64} string (%s)`, (base64String: string, utf8String: string) => {
       const result: string = decode(base64String, BASE64);
       expect(result).toBe(utf8String);
     });
 
-    test("Decoding an empty base64 string", () => {
+    test(`Decoding an empty ${BASE64} string`, () => {
       const result: string = decode(EMPTY_STRING, BASE64);
       expect(result).toBe(EMPTY_STRING);
     });
@@ -34,19 +34,19 @@ describe("::decode", () => {
 
 describe("::encode", () => {
 
-  describe("Base64", () => {
+  describe(`Encoding to ${BASE64}`, () => {
 
     test.each([
       ["Hello world!", "SGVsbG8gd29ybGQh"],
       ["banana for scale", "YmFuYW5hIGZvciBzY2FsZQ=="],
       ["Another one bites de_dust", "QW5vdGhlciBvbmUgYml0ZXMgZGVfZHVzdA=="],
       ["The Spanish Inquisition", "VGhlIFNwYW5pc2ggSW5xdWlzaXRpb24="],
-    ])("Encode a base64 string (%s)", (utf8String: string, base64String: string) => {
+    ])(`Encode a ${BASE64} string (%s)`, (utf8String: string, base64String: string) => {
       const result: string = encode(utf8String, BASE64);
       expect(result).toBe(base64String);
     });
 
-    test("Encoding an empty string to base64", () => {
+    test(`Encoding an empty string to ${BASE64}`, () => {
       const result: string = encode(EMPTY_STRING, BASE64);
       expect(result).toBe(EMPTY_STRING);
     });
