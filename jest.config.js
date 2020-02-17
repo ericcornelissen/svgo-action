f529c5b971001a369b86b3649282f282a49931b4
Eric Cornelisesn <ericornelissen@gmail.com>
2020-02-17 11:13:01 +0200

    chore: remove irrelevant coverage from report

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+
+  // Coverage reporting
   coverageDirectory: "_reports/coverage",
+  coveragePathIgnorePatterns: [
+    "lib",
+    "node_modules",
+    "test/mocks",
+  ],
 };
