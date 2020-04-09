a890177d97e8fad7826c0d2ef017fc762b9571d4
Eric Cornelisesn <ericornelissen@gmail.com>
2020-04-09 23:23:50 +0300

    test: utility functions (on their own)

---
+++ test/fetch-yaml.test.ts
@@ -0,0 +1,33 @@
+import files from "./fixtures/file-data.json";
+
+import * as core from "./mocks/@actions/core.mock";
+import * as github from "./mocks/@actions/github.mock";
+
+jest.mock("@actions/core", () => core);
+jest.mock("@actions/github", () => github);
+
+import { fetchYamlFile } from "../src/utils/fetch-yaml";
+
+
+const token = core.getInput("repo-token", { required: true });
+const client = new github.GitHub(token);
+
+
+describe("::fetchYamlFile", () => {
+
+  const testFiles = test.each(
+    Object.keys(files)
+      .filter((filePath) => filePath.endsWith(".yml")),
+  );
+
+  test("an unknown file", async () => {
+    const result = await fetchYamlFile(client, "this is definitely not a file");
+    expect(result).toEqual({ });
+  });
+
+  testFiles("a file in the repository", async (filePath) => {
+    const result = await fetchYamlFile(client, filePath);
+    expect(result).not.toEqual({ });
+  });
+
+});
