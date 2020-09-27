f8a689a7116c726820be90db4d5b9b26ca9f9e6f
Eric Cornelissen <ericornelissen@gmail.com>
2020-09-27 16:10:16 +0300

    refactor: outline behaviour in schedule context

--- src/events/schedule.ts
+++ src/events/schedule.ts
@@ -3,11 +3,36 @@ import { Octokit } from "@octokit/core";
 
 import { ActionConfig } from "../inputs";
 import { SVGOptimizer } from "../svgo";
+import { ContextData, GitFileInfo } from "../types";
+import {
+  getCommitData,
+  doCommit,
+  doFilterSvgsFromFiles,
+  doOptimizeSvgs,
+} from "./helpers";
+
+
+function getDefaultRef(): string {
+  return "master";
+}
+
+async function getSvgsInRepo(
+  client: Octokit,
+  ignoreGlob: string,
+): Promise<ContextData> {
+  core.debug("fetching changed files for pushed commits");
+  const files: GitFileInfo[] = [];
+  return doFilterSvgsFromFiles(client, files, ignoreGlob);
+}
+
 
 export default async function main(
   client: Octokit,
   config: ActionConfig,
   svgo: SVGOptimizer,
 ): Promise<void> {
-  core.info("TO BE IMPLEMENTED");
+  const context = await getSvgsInRepo(client, config.ignoreGlob);
+  const optimizedSvgs = await doOptimizeSvgs(svgo, context.svgs);
+  const commitData = getCommitData(context, optimizedSvgs);
+  await doCommit(client, getDefaultRef(), config, commitData);
 }
