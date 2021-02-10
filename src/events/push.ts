91e52ead04f2bedbf1116b3525a89d1fabd5b25d
Eric Cornelissen <ericornelissen@gmail.com>
2021-02-10 19:22:10 +0100

    refactor: optimize local SVGs in push context

--- src/events/push.ts
+++ src/events/push.ts
@@ -1,26 +1,17 @@
-import * as core from "@actions/core";
-import * as github from "@actions/github";
 import { Octokit } from "@octokit/core";
+import * as path from "path";
+import * as fs from "fs";
 
 import {
-  DISABLE_PATTERN,
   OUTPUT_NAME_DID_OPTIMIZE,
   OUTPUT_NAME_OPTIMIZED_COUNT,
   OUTPUT_NAME_SKIPPED_COUNT,
   OUTPUT_NAME_SVG_COUNT,
 } from "../constants";
-import { removedFiles } from "../filters";
-import { getCommitFiles } from "../github-api";
 import { ActionConfig } from "../inputs";
 import { SVGOptimizer } from "../svgo";
-import { ContextData, GitFileInfo, OutputName } from "../types";
-import {
-  getCommitData,
-  doCommit,
-  doFilterSvgsFromFiles,
-  doOptimizeSvgs,
-  setOutputValues,
-} from "./common";
+import { OutputName } from "../types";
+import { setOutputValues } from "./common";
 
 
 const OUTPUT_NAMES: OutputName[] = [
@@ -30,81 +21,56 @@ const OUTPUT_NAMES: OutputName[] = [
   OUTPUT_NAME_SVG_COUNT,
 ];
 
-function getHeadRef(): string {
-  return github.context.payload.ref.replace("refs/", "");
-}
-
-function isDisabledForCommit(commitMessage: string): boolean {
-  return DISABLE_PATTERN.test(commitMessage);
-}
-
-function samePathAs(ref: GitFileInfo): ((subject: GitFileInfo) => boolean) {
-  return function (subject: GitFileInfo): boolean {
-    return subject.path === ref.path;
-  };
-}
-
-function removed(
-  file: GitFileInfo,
-  index: number,
-  arr: GitFileInfo[],
-): boolean {
-  const subsequentEntries: GitFileInfo[] = arr.slice(index);
-  const removedEntries: GitFileInfo[] = subsequentEntries.filter(removedFiles);
-  const removedIndex: number = removedEntries.findIndex(samePathAs(file));
-  return removedIndex === -1;
-}
-
-function duplicates(
-  file: GitFileInfo,
-  index: number,
-  arr: GitFileInfo[],
-): boolean {
-  const subsequentEntries: GitFileInfo[] = arr.slice(index + 1);
-  const duplicateIndex: number = subsequentEntries.findIndex(samePathAs(file));
-  return duplicateIndex === -1;
-}
-
-async function getFilesInCommits(client: Octokit): Promise<GitFileInfo[]> {
-  const commitsFiles: GitFileInfo[] = [];
-
-  const commits = github.context.payload.commits;
-  for (const commit of commits) {
-    const commitId: string = commit.id;
-
-    const commitMessage: string = commit.message;
-    if (isDisabledForCommit(commitMessage)) {
-      core.info(`Action disabled for commit ${commitId}`);
+function* listFilesRecursively(dirPath: string) {
+  for (const entry of fs.readdirSync(dirPath)) {
+    if ([".git"].includes(entry)) {
       continue;
     }
 
-    const commitFiles: GitFileInfo[] = await getCommitFiles(client, commitId);
-    commitsFiles.push(...commitFiles);
+    const entryPath = path.resolve(dirPath, entry);
+    if (!fs.existsSync(entryPath)) {
+      continue;
+    }
+
+    const lstat = fs.lstatSync(entryPath);
+    if (lstat.isFile()) {
+      yield entryPath;
+    } else {
+      yield* listFilesRecursively(entryPath);
+    }
   }
-
-  return commitsFiles.filter(removed).filter(duplicates);
 }
-
-async function getSvgsInCommits(
-  client: Octokit,
-  commitSha: string,
-  ignoreGlob: string,
-): Promise<ContextData> {
-  core.debug("fetching changed files for pushed commits");
-  const files: GitFileInfo[] = await getFilesInCommits(client);
-  return doFilterSvgsFromFiles(client, commitSha, files, ignoreGlob);
-}
-
 
 export default async function main(
-  client: Octokit,
+  _: Octokit,
   config: ActionConfig,
   svgo: SVGOptimizer,
 ): Promise<void> {
-  const commitSha: string = github.context.sha;
-  const context = await getSvgsInCommits(client, commitSha, config.ignoreGlob);
-  const optimizedSvgs = await doOptimizeSvgs(svgo, context.svgs);
-  const commitData = getCommitData(context, optimizedSvgs);
-  await doCommit(client, getHeadRef(), config, commitData);
-  setOutputValues(commitData, OUTPUT_NAMES);
+  let optimizedCount = 0, svgCount = 0;
+
+  for (const filePath of listFilesRecursively(".")) {
+    if (path.extname(filePath) !== ".svg") {
+      continue;
+    }
+
+    const originalContent = fs.readFileSync(filePath).toString();
+    const optimizedContent = await svgo.optimize(originalContent);
+    fs.writeFileSync(filePath, optimizedContent);
+
+    optimizedCount++;
+    svgCount++;
+  }
+
+  setOutputValues({
+    fileCount: 0,
+    fileData: {
+      optimized: [],
+      original: [],
+    },
+    ignoredCount: 0,
+    optimizedCount: optimizedCount,
+    skippedCount: 0,
+    svgCount: svgCount,
+    warnings: [],
+  }, OUTPUT_NAMES);
 }
