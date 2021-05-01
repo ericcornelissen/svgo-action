8580bd7a7bfb9e39a639404e38582dbf5f07c38c
Eric Cornelissen <ericornelissen@gmail.com>
2021-05-01 20:53:58 +0200

    test: update tests for removal external configuration file

--- test/inputs.test.ts
+++ test/inputs.test.ts
@@ -9,7 +9,6 @@ import {
   INPUT_NAME_SVGO_VERSION,
 } from "../src/constants";
 import { ActionConfig } from "../src/inputs";
-import { RawActionConfig } from "../src/types";
 
 function mockCoreGetInput(key: string, value: string): void {
   when(core.getInput)
@@ -23,15 +22,6 @@ describe("ActionConfig", () => {
   describe("::constructor", () => {
     test("construct without a parameter", () => {
       expect(() => new ActionConfig(core)).not.toThrow();
-    });
-
-    test("construct without an empty object", () => {
-      expect(() => new ActionConfig(core, { })).not.toThrow();
-    });
-
-    test("construct with an object specifying dry-run", () => {
-      const config: RawActionConfig = { "dry-run": true };
-      expect(() => new ActionConfig(core, config)).not.toThrow();
     });
   });
 
@@ -64,52 +54,12 @@ describe("ActionConfig", () => {
       const instance: ActionConfig = new ActionConfig(core);
       expect(instance.isDryRun).toBe(true);
     });
-
-    test("dry-run is `false` in the config object", () => {
-      const rawConfig = { "dry-run": false };
-      mockCoreGetInput(INPUT_NAME_DRY_RUN, "true");
-
-      const instance: ActionConfig = new ActionConfig(core, rawConfig);
-      expect(instance.isDryRun).toBe(false);
-    });
-
-    test("dry-run is `true` in the config object", () => {
-      const rawConfig = { "dry-run": true };
-      mockCoreGetInput(INPUT_NAME_DRY_RUN, "false");
-
-      const instance: ActionConfig = new ActionConfig(core, rawConfig);
-      expect(instance.isDryRun).toBe(true);
-    });
-
-    test("dry-run is `'false'` in the config object", () => {
-      const rawConfig = { "dry-run": "false" } as unknown as RawActionConfig;
-      mockCoreGetInput(INPUT_NAME_DRY_RUN, "true");
-
-      const instance: ActionConfig = new ActionConfig(core, rawConfig);
-      expect(instance.isDryRun).toBe(false);
-    });
-
-    test("dry-run is `'true'` in the config object", () => {
-      const rawConfig = { "dry-run": "true" } as unknown as RawActionConfig;
-      mockCoreGetInput(INPUT_NAME_DRY_RUN, "false");
-
-      const instance: ActionConfig = new ActionConfig(core, rawConfig);
-      expect(instance.isDryRun).toBe(true);
-    });
-
-    test.each(nonBooleanStrings)("dry-run is `'%s'` in the config object", (value) => {
-      const rawConfig = { "dry-run": value } as unknown as RawActionConfig;
-      mockCoreGetInput(INPUT_NAME_DRY_RUN, "false");
-
-      const instance: ActionConfig = new ActionConfig(core, rawConfig);
-      expect(instance.isDryRun).toBe(true);
-    });
   });
 
   describe(".ignoreGlob", () => {
     const globs: string[] = ["file.svg", "dir/*", "folder/**/*"];
 
-    test("dry-run is not set at all", () => {
+    test("ignore is not set at all", () => {
       const defaultValue = "";
       mockCoreGetInput(INPUT_NAME_IGNORE, defaultValue);
 
@@ -122,27 +72,6 @@ describe("ActionConfig", () => {
 
       const instance: ActionConfig = new ActionConfig(core);
       expect(instance.ignoreGlob).toBe(glob);
-    });
-
-    test.each(globs)("ignore is '%s' in the config file", (glob) => {
-      const defaultValue = "";
-      const rawConfig: RawActionConfig = { ignore: glob };
-      mockCoreGetInput(INPUT_NAME_IGNORE, defaultValue);
-
-      const instance: ActionConfig = new ActionConfig(core, rawConfig);
-      expect(instance.ignoreGlob).toBe(glob);
-    });
-
-    test.each([
-      ["foo.svg", "bar.svg"],
-      ["dir/*", "folder/*"],
-      ["folder/**/*", "dir/**/*"],
-    ])("ignore is '%s' in the workflow file and '%s' in the config file", (workflowGlob, configGlob) => {
-      const rawConfig: RawActionConfig = { ignore: configGlob };
-      mockCoreGetInput(INPUT_NAME_DRY_RUN, workflowGlob);
-
-      const instance: ActionConfig = new ActionConfig(core, rawConfig);
-      expect(instance.ignoreGlob).toBe(configGlob);
     });
   });
 
@@ -161,13 +90,6 @@ describe("ActionConfig", () => {
       mockCoreGetInput(INPUT_NAME_SVGO_OPTIONS, path);
 
       const instance: ActionConfig = new ActionConfig(core);
-      expect(instance.svgoOptionsPath).toBe(path);
-    });
-
-    test.each(paths)("svgo-options is set (to '%s') in the config object", (path) => {
-      mockCoreGetInput(INPUT_NAME_SVGO_OPTIONS, `dir/${path}`);
-
-      const instance: ActionConfig = new ActionConfig(core, { "svgo-options": path });
       expect(instance.svgoOptionsPath).toBe(path);
     });
   });
@@ -200,63 +122,6 @@ describe("ActionConfig", () => {
       mockCoreGetInput(INPUT_NAME_SVGO_VERSION, "3");
 
       const instance: ActionConfig = new ActionConfig(core);
-      expect(instance.svgoVersion).toBe(2);
-    });
-
-    test("set to `1` in config file", () => {
-      mockCoreGetInput(INPUT_NAME_SVGO_VERSION, defaultValue);
-      const rawConfig: RawActionConfig = { "svgo-version": 1 };
-
-      const instance: ActionConfig = new ActionConfig(core, rawConfig);
-      expect(instance.svgoVersion).toBe(1);
-    });
-
-    test("set to `2` in config file", () => {
-      mockCoreGetInput(INPUT_NAME_SVGO_VERSION, defaultValue);
-      const rawConfig: RawActionConfig = { "svgo-version": 2 };
-
-      const instance: ActionConfig = new ActionConfig(core, rawConfig);
-      expect(instance.svgoVersion).toBe(2);
-    });
-
-    test("set to `'1'` in config file", () => {
-      mockCoreGetInput(INPUT_NAME_SVGO_VERSION, defaultValue);
-      const rawConfig = { "svgo-version": "1" } as unknown as RawActionConfig;
-
-      const instance: ActionConfig = new ActionConfig(core, rawConfig);
-      expect(instance.svgoVersion).toBe(1);
-    });
-
-    test("set to `'2'` in config file", () => {
-      mockCoreGetInput(INPUT_NAME_SVGO_VERSION, defaultValue);
-      const rawConfig = { "svgo-version": "2" } as unknown as RawActionConfig;
-
-      const instance: ActionConfig = new ActionConfig(core, rawConfig);
-      expect(instance.svgoVersion).toBe(2);
-    });
-
-    test("set to neither `1` or `2` in config file", () => {
-      const defaultValue = "1";
-      mockCoreGetInput(INPUT_NAME_SVGO_VERSION, defaultValue);
-      const rawConfig: RawActionConfig = { "svgo-version": 3 };
-
-      const instance: ActionConfig = new ActionConfig(core, rawConfig);
-      expect(instance.svgoVersion).toBe(2);
-    });
-
-    test("set to `1` in config file and something else in workflow file", () => {
-      mockCoreGetInput(INPUT_NAME_SVGO_VERSION, "2");
-      const rawConfig: RawActionConfig = { "svgo-version": 1 };
-
-      const instance: ActionConfig = new ActionConfig(core, rawConfig);
-      expect(instance.svgoVersion).toBe(1);
-    });
-
-    test("set to `2` in config file and something else in workflow file", () => {
-      mockCoreGetInput(INPUT_NAME_SVGO_VERSION, "1");
-      const rawConfig: RawActionConfig = { "svgo-version": 2 };
-
-      const instance: ActionConfig = new ActionConfig(core, rawConfig);
       expect(instance.svgoVersion).toBe(2);
     });
   });
