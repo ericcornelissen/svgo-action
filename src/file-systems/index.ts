4786ff2ae96876a63e84b7e5af7769d48997303f
Eric Cornelissen <ericornelissen@gmail.com>
2021-07-22 11:37:33 +0200

    feat: detect and only optimize SVGs in pushed commits

--- src/file-systems/index.ts
+++ src/file-systems/index.ts
@@ -2,12 +2,14 @@ import type { GitHubClient } from "../clients";
 import type { error, Context } from "../types";
 import type { FileInfo, FileSystem } from "./types";
 import type { PrContext } from "./pr";
+import type { PushContext } from "./push";
 
 import * as path from "path";
 
 import { EVENTS } from "../constants";
 import { BaseFileSystem } from "./base";
 import { createPrFileSystemBuilder } from "./pr";
+import { createPushFileSystemBuilder } from "./push";
 
 interface Params {
   readonly client: GitHubClient;
@@ -15,6 +17,7 @@ interface Params {
 }
 
 const newPullRequest = createPrFileSystemBuilder({ fs: BaseFileSystem, path });
+const newPush = createPushFileSystemBuilder({ fs: BaseFileSystem, path });
 const newStandard = (): FileSystem => BaseFileSystem;
 
 async function New({ client, context }: Params): Promise<[FileSystem, error]> {
@@ -23,6 +26,9 @@ async function New({ client, context }: Params): Promise<[FileSystem, error]> {
   switch (context.eventName) {
     case EVENTS.pullRequest:
       [fileSystem, err] = await newPullRequest(client, context as PrContext);
+      break;
+    case EVENTS.push:
+      [fileSystem, err] = await newPush(client, context as PushContext);
       break;
     default:
       fileSystem = newStandard();
