91047b2b347a71c6d82de04a57bbaac3ffd33606
Eric Cornelissen <ericornelissen@gmail.com>
2021-08-14 10:57:07 +0200

    refactor: extract default input values into constants package

--- src/inputs/index.ts
+++ src/inputs/index.ts
@@ -1,6 +1,13 @@
 import type { error, Inputter } from "../types";
 import type { Config } from "./types";
 
+import {
+  DEFAULT_IGNORE_GLOB,
+  DEFAULT_IS_DRY_RUN,
+  DEFAULT_SVGO_V1_CONFIG_PATH,
+  DEFAULT_SVGO_V2_CONFIG_PATH,
+  DEFAULT_SVGO_VERSION,
+} from "../constants";
 import errors from "../errors";
 import {
   getIgnoreGlob,
@@ -13,14 +20,21 @@ interface Params {
   readonly inp: Inputter;
 }
 
+function getDefaultSvgoConfigPath(svgoVersion: number): string {
+  if (svgoVersion === 1) {
+    return DEFAULT_SVGO_V1_CONFIG_PATH;
+  }
+
+  return DEFAULT_SVGO_V2_CONFIG_PATH;
+}
+
 function New({ inp }: Params): [Config, error] {
-  const [ignoreGlob, err0] = getIgnoreGlob(inp, "");
-  const [isDryRun, err1] = getIsDryRun(inp, false);
-  const [svgoVersion, err3] = getSvgoVersion(inp, 2);
-  const [svgoConfigPath, err2] = getSvgoConfigPath(
-    inp,
-    svgoVersion === 2 ? "svgo.config.js" : ".svgo.yml",
-  );
+  const [ignoreGlob, err0] = getIgnoreGlob(inp, DEFAULT_IGNORE_GLOB);
+  const [isDryRun, err1] = getIsDryRun(inp, DEFAULT_IS_DRY_RUN);
+  const [svgoVersion, err3] = getSvgoVersion(inp, DEFAULT_SVGO_VERSION);
+
+  const defaultSvgoConfigPath = getDefaultSvgoConfigPath(svgoVersion);
+  const [svgoConfigPath, err2] = getSvgoConfigPath(inp, defaultSvgoConfigPath);
 
   return [
     {
