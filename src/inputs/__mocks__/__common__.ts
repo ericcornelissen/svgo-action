cb1d0931fc7c90cd4403552aa10c90a00221dcb5
Eric Cornelissen <ericornelissen@gmail.com>
2021-09-15 11:33:41 +0200

    chore: update package inputs __mocks__

---
+++ src/inputs/__mocks__/__common__.ts
@@ -0,0 +1,10 @@
+const defaultConfig = {
+  ignoreGlobs: { provided: true, value: [] },
+  isDryRun: { provided: true, value: false },
+  svgoConfigPath: { provided: true, value: "svgo.config.js" },
+  svgoVersion: { provided: true, value: 2 },
+};
+
+export {
+  defaultConfig,
+};
