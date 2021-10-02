2f8c5d31bd446aa6862bf16e713917dedd46ca10
Eric Cornelissen <ericornelissen@gmail.com>
2021-10-02 15:51:25 +0200

    chore: remove unused type documentation

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,5 @@
 import type { GitHub as _GitHub } from "@actions/github/lib/utils";
 
-// Type representing the relevant API of `@actions/github.context`.
 interface Context {
   readonly eventName: string;
   readonly payload: {
@@ -17,7 +16,6 @@ interface Context {
   };
 }
 
-// Type representing the relevant API of `@actions/core`.
 interface Core extends Inputter, Outputter {
   debug(message: string): void;
   info(message: string): void;
@@ -25,31 +23,25 @@ interface Core extends Inputter, Outputter {
   warning(message: string | Error): void;
 }
 
-// Type for errors.
 type error = string | null;
 
-// Type representing the relevant API of `@actions/github`.
 interface GitHub {
   readonly context: Context;
   getOctokit(token: string): Octokit;
 }
 
-// Type representing an object from which the Action inputs can be obtained.
 interface Inputter {
   getBooleanInput(name: string, options: InputterOptions): boolean;
   getInput(name: string, options: InputterOptions): string;
   getMultilineInput(name: string, options: InputterOptions): string[];
 }
 
-// Type representing the options for the `Inputter` interface.
 interface InputterOptions {
   readonly required?: boolean;
 }
 
-// Type representing `@actions/github`'s Octokit.
 type Octokit = InstanceType<typeof _GitHub>;
 
-// Type representing an object to which Action outputs can be outputted.
 interface Outputter {
   setOutput(name: string, value: string): void;
 }
