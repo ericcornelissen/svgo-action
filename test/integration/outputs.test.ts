f6f8ea904c96666a703187591e6fe4528d278e0a
Eric Cornelissen <ericornelissen@gmail.com>
2021-11-27 17:39:39 +0100

    test: improve integration suite for package outputs

--- test/integration/outputs.test.ts
+++ test/integration/outputs.test.ts
@@ -2,36 +2,104 @@ import out from "../__common__/outputter.mock";
 
 import outputs from "../../src/outputs";
 
-describe("package outputs", () => {
+describe.each([
+  { optimizedCount: 1, svgCount: 4 },
+  { optimizedCount: 7, svgCount: 18 },
+  { optimizedCount: 0, svgCount: 3 },
+])("package outputs", ({ optimizedCount, svgCount }) => {
+  const didOptimize = optimizedCount > 0;
+  const data = {
+    optimizedCount,
+    svgCount,
+  };
+
+  const DID_OPTIMIZE = "DID_OPTIMIZE";
+  const OPTIMIZED_COUNT = "OPTIMIZED_COUNT";
+  const SVG_COUNT = "SVG_COUNT";
+
   const EVENT_PULL_REQUEST = "pull_request";
   const EVENT_PUSH = "push";
   const EVENT_REPOSITORY_DISPATCH = "repository_dispatch";
   const EVENT_SCHEDULE = "schedule";
   const EVENT_WORKFLOW_DISPATCH = "workflow_dispatch";
 
-  const data = {
-    optimizedCount: 3,
-    svgCount: 14,
-  };
+  beforeEach(() => {
+    out.setOutput.mockReset();
+  });
 
-  test.each([
+  describe.each([
     EVENT_PULL_REQUEST,
     EVENT_PUSH,
     EVENT_REPOSITORY_DISPATCH,
     EVENT_SCHEDULE,
     EVENT_WORKFLOW_DISPATCH,
   ])("known event ('%s')", (eventName) => {
-    const env = { context: { eventName } };
+    const env = {
+      context: {
+        eventName,
+      },
+    };
 
-    const err = outputs.Set({ env, data, out });
-    expect(err).toBeNull();
-    expect(out.setOutput).toHaveBeenCalled();
+    test("did optimize", () => {
+      const err = outputs.Set({ env, data, out });
+
+      expect(err).toBeNull();
+      expect(out.setOutput).toHaveBeenCalledWith(
+        DID_OPTIMIZE,
+        `${didOptimize}`,
+      );
+    });
+
+    test("optimized count", () => {
+      const err = outputs.Set({ env, data, out });
+
+      expect(err).toBeNull();
+      expect(out.setOutput).toHaveBeenCalledWith(
+        OPTIMIZED_COUNT,
+        `${optimizedCount}`,
+      );
+    });
+
+    test("svg count", () => {
+      const err = outputs.Set({ env, data, out });
+
+      expect(err).toBeNull();
+      expect(out.setOutput).toHaveBeenCalledWith(SVG_COUNT, `${svgCount}`);
+    });
   });
 
-  test("unknown event", () => {
-    const env = { context: { eventName: "foobar" } };
+  describe("unknown event", () => {
+    const env = {
+      context: {
+        eventName: "uNkNoWn EvEnT",
+      },
+    };
 
-    const err = outputs.Set({ env, data, out });
-    expect(err).toBeNull();
+    test("did optimize", () => {
+      const err = outputs.Set({ env, data, out });
+
+      expect(err).toBeNull();
+      expect(out.setOutput).toHaveBeenCalledWith(
+        DID_OPTIMIZE,
+        `${didOptimize}`,
+      );
+    });
+
+    test("optimized count", () => {
+      const err = outputs.Set({ env, data, out });
+
+      expect(err).toBeNull();
+      expect(out.setOutput).toHaveBeenCalledWith(
+        OPTIMIZED_COUNT,
+        `${optimizedCount}`,
+      );
+    });
+
+    test("svg count", () => {
+      const err = outputs.Set({ env, data, out });
+
+      expect(err).toBeNull();
+      expect(out.setOutput).toHaveBeenCalledWith(SVG_COUNT, `${svgCount}`);
+    });
   });
 });
