d1bd4eef460f8de23ca8f79f377b427c8fbd9aae
Eric Cornelissen <ericornelissen@gmail.com>
2021-11-27 16:51:40 +0100

    refactor: simplify package outputs

--- src/outputs/names.ts
+++ src/outputs/names.ts
@@ -1,13 +1,5 @@
 import type { error } from "../types";
 
-import {
-  EVENT_PULL_REQUEST,
-  EVENT_PUSH,
-  EVENT_REPOSITORY_DISPATCH,
-  EVENT_SCHEDULE,
-  EVENT_WORKFLOW_DISPATCH,
-} from "./constants";
-
 const enum OutputName {
   DID_OPTIMIZE = "DID_OPTIMIZE",
   OPTIMIZED_COUNT = "OPTIMIZED_COUNT",
@@ -16,11 +8,6 @@ const enum OutputName {
 
 function getOutputNamesFor(event: string): [OutputName[], error] {
   switch (event) {
-  case EVENT_PULL_REQUEST:
-  case EVENT_PUSH:
-  case EVENT_REPOSITORY_DISPATCH:
-  case EVENT_SCHEDULE:
-  case EVENT_WORKFLOW_DISPATCH:
   default:
     return [[
       OutputName.DID_OPTIMIZE,
