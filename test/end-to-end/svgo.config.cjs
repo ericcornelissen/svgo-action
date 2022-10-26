6356d1bd0e236b64aa21582925eb36694d9ac2a1
Eric Cornelissen <ericornelissen@gmail.com>
2022-10-26 21:59:01 +0200

    test: update e2e test's svgo config file for v2 and v3
    Omit plugins that were renamed in SVGO v3 to prevent errors due to an
    invalid configuration.

--- test/end-to-end/svgo.config.cjs
+++ test/end-to-end/svgo.config.cjs
@@ -11,8 +11,6 @@ module.exports = {
     "inlineStyles",
     "minifyStyles",
     "convertStyleToAttrs",
-    "cleanupIDs",
-    "prefixIds",
     "removeRasterImages",
     "removeUselessDefs",
     "cleanupNumericValues",
