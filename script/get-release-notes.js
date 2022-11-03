f3a0227ea31a560df8019c1b5d102585c1dc14b1
Eric Cornelissen <ericornelissen@gmail.com>
2022-11-03 20:52:40 +0100

    chore: refactor out common logic in scripts

--- script/get-release-notes.js
+++ script/get-release-notes.js
@@ -1,26 +1,23 @@
-import fs from "node:fs";
-import path from "node:path";
 import process from "node:process";
 
-const manifestFile = path.resolve("./package.json");
-const changelogFile = path.resolve("./CHANGELOG.md");
+import {
+  getChangelog,
+  getCurrentVersionHeader,
+  isCurrentVersionReleased,
+} from "./helpers.js";
 
-const manifestRaw = fs.readFileSync(manifestFile).toString();
-const manifest = JSON.parse(manifestRaw);
-const version = manifest.version;
-const versionHeader = `## [${version}]`;
-
-const changelog = fs.readFileSync(changelogFile).toString();
-if (!changelog.includes(versionHeader)) {
-  throw new Error(`${version} missing from CHANGELOG`);
+if (!isCurrentVersionReleased()) {
+  throw new Error("The current version in the manifest hasn't been released");
 }
 
-const startIndex = changelog.indexOf(versionHeader) + versionHeader.length + 13;
-const endIndex = startIndex + (
-  changelog
-    .substring(startIndex)
-    .indexOf("## [")
-);
+const changelog = getChangelog();
+const versionHeader = getCurrentVersionHeader();
+
+const startIndex = changelog.indexOf(versionHeader)
+  + versionHeader.length
+  + " - 2022-01-01".length;
+const endIndex = startIndex
+  + changelog.substring(startIndex).indexOf("## [");
 
 const releaseNotes = changelog.substring(startIndex, endIndex);
 process.stdout.write(releaseNotes);
---
+++ script/helpers.js
@@ -0,0 +1,81 @@
+import fs from "node:fs";
+import path from "node:path";
+
+/**
+ * Get the project's changelog.
+ *
+ * @returns {string} The changelog.
+ */
+export function getChangelog() {
+  const changelogFilePath = getPathTo("CHANGELOG.md");
+  const changelogRaw = readFile(changelogFilePath);
+  return changelogRaw;
+}
+
+/**
+ * Get the changelog version header for the current version
+ *
+ * @returns {string} The version header.
+ */
+export function getCurrentVersionHeader() {
+  const manifest = getManifest();
+  const version = manifest.version;
+  const versionHeader = `## [${version}]`;
+  return versionHeader;
+}
+
+/**
+ * Get the project's manifest.
+ *
+ * @returns {Record<string, any>} The manifest as a JavaScript.
+ */
+export function getManifest() {
+  const manifestFilePath = getPathTo("package.json");
+  const manifestRaw = readFile(manifestFilePath);
+  const manifest = JSON.parse(manifestRaw);
+  return manifest;
+}
+
+/**
+ * Get the path to a file in the project.
+ *
+ * @param {string} relativePath A project-root relative file path.
+ * @returns {string} The absolute path
+ */
+function getPathTo(relativePath) {
+  const absolutePath = path.resolve(".", relativePath);
+  return absolutePath;
+}
+
+/**
+ * Check if the current version stated in the package manifest has been
+ * released according to the changelog.
+ *
+ * @returns {boolean} `true` if released, `false` otherwise.
+ */
+export function isCurrentVersionReleased() {
+  const changelog = getChangelog();
+  const versionHeader = getCurrentVersionHeader();
+  return changelog.includes(versionHeader);
+}
+
+/**
+ * Read a file.
+ *
+ * @param {string} file An absolute file path.
+ * @returns {string} The file contents.
+ */
+function readFile(filepath) {
+  const content = fs.readFileSync(filepath).toString();
+  return content;
+}
+
+/**
+ * Rewrite the project's changelog.
+ *
+ * @param {string} changelog The new changelog contents.
+ */
+export function writeChangelog(changelog) {
+  const changelogFile = getPathTo("CHANGELOG.md");
+  fs.writeFileSync(changelogFile, changelog);
+}
