50916d0264232b4bbd1d1d28833a5e999e9fe1c7
Eric Cornelissen <ericornelissen@gmail.com>
2023-12-31 18:17:19 +0100

    chore: update deprecation notice

--- src/helpers/deprecation.ts
+++ src/helpers/deprecation.ts
@@ -10,9 +10,8 @@ function deprecationWarnings({
   core,
 }: Params): void {
   core.warning(
-    "General support for SVGO Action v3 ended 2023-09-23. Security " +
-    "updates will be supported until 2023-12-31. Please upgrade to SVGO " +
-    "Action v4 as soon as possible.",
+    "Support for SVGO Action v3 ended 2023-12-31. Please upgrade to the " +
+    "latest version as soon as possible",
   );
 }
 
--- test/unit/helpers/deprecation.test.ts
+++ test/unit/helpers/deprecation.test.ts
@@ -16,9 +16,8 @@ describe("helpers/deprecation.ts", () => {
   test("deprecation warning for v3 of the Action", () => {
     deprecationWarnings({ core });
     expect(core.warning).toHaveBeenCalledWith(
-      "General support for SVGO Action v3 ended 2023-09-23. Security " +
-      "updates will be supported until 2023-12-31. Please upgrade to SVGO " +
-      "Action v4 as soon as possible.",
+      "Support for SVGO Action v3 ended 2023-12-31. Please upgrade to the " +
+      "latest version as soon as possible",
     );
   });
 });
